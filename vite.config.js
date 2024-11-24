32609b7721ce807f01966a8f431d584510a319fb
ParthaRana <parthacrana@gmail.com>
2024-11-24 13:14:08 -0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-react-portfolio"
 })
 
 // open this in integrated terminal and type: npm run dev -- to run react app
